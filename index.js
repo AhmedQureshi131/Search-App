4ce0d81fcc8016a6fcd62c9f5ac333389ece4db2
Ahmed Ali Qureshi <55103689+AhmedQureshi131@users.noreply.github.com>
2020-06-24 01:45:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-search: function(searchterm, searchLimit, sortBy){
-        return fetch(`http://www.reddit.com/search.json?q=${searchterm}&sort=${sortBy}&limit=${searchLimit}`)
-        //we want response in json
-        .then(res => res.json())
-        //now we can get the data
-        .then(data => data.data.children.map(data => data.data)).catch(err => console.log(err));
-    }
+
 
 
 
@@ -86,3 +80,10 @@ function truncateText(text, limit){
    if(shortend == -1) return text;
    return text.substring(0, shortend);
 }
+function search(searchterm, searchLimit, sortBy){
+        return fetch(`http://www.reddit.com/search.json?q=${searchterm}&sort=${sortBy}&limit=${searchLimit}`)
+        //we want response in json
+        .then(res => res.json())
+        //now we can get the data
+        .then(data => data.data.children.map(data => data.data)).catch(err => console.log(err));
+    }
